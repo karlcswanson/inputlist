44e5d9fa48c7f8a0351e313528dd3978d24e3271
Karl Swanson <karlcswanson@gmail.com>
2017-05-27 22:18:09 +0000

    Fixed error caused when no wwb.csv is present

--- routes/scan.js
+++ routes/scan.js
@@ -6,10 +6,15 @@ var last_modified;
 
 
 router.get('/', function(req, res, next) {
-  var stats = fs.statSync(__basedir + "/public/scans/wwb.csv");
-  last_modified = moment(stats['mtime']);
-  last_modified = last_modified.fromNow();
-  res.render('scan', { title: 'TSC | Frequency Scan', last_modified: last_modified });
+  try {
+    var stats = fs.statSync(__basedir + "/public/scans/wwb.csv");
+    last_modified = moment(stats['mtime']);
+    last_modified = last_modified.fromNow();
+    res.render('scan', { title: 'TSC | Frequency Scan', last_modified: last_modified });
+  }
+  catch (err) {
+    res.render('scan_error', { title: 'TSC | Frequency Scan'});
+  }
 });
 
 module.exports = router;
