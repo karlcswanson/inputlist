109bec2db3aedde6e72840f3141cf7ba6e77959b
Karl Swanson <karlcswanson@gmail.com>
2017-05-31 23:25:43 +0000

    Resolved error with first load of /network

--- routes/network.js
+++ routes/network.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
-var net_scan = require('../public/scans/net_scan.json')
+var fs = require('fs');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var last_modified = moment(net_scan["last_check"]);
-  last_modified = last_modified.fromNow();
-  res.render('network', { title: 'TSC | Network', 'net_scan': net_scan, last_modified:last_modified });
+  fs.readFile(__basedir + '/public/scans/net_scan.json','utf8', function(error, data){
+    if (error) {
+      res.render('net_scan_error', { title: 'TSC | Network'});
+    } else {
+      net_scan = JSON.parse(data);
+      var last_modified = moment(net_scan["last_check"]);
+      last_modified = last_modified.fromNow();
+      res.render('network', { title: 'TSC | Network', 'net_scan': net_scan, last_modified:last_modified });
+    }
+  });
 });
 
 module.exports = router;
